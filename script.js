29d76dbe3418e5fdb0913508aab227eb23a8c5bb
From 0 to 1 <101225909+VitaliPri@users.noreply.github.com>
2023-11-25 20:04:29 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,31 @@
 ///
+const counter = (a_or_s) => { let m = new Map(); for (const x of a_or_s) m.set(x, m.get(x) + 1 || 1); return m; };
+
+const mod = 1e9 + 7;
+const countSubMultisets = (a, l, r) => {
+    let f = Array(r + 1).fill(0), m = counter(a), res = 0;
+    f[0] = 1;
+    for (const [x, occ] of m) {
+        if (x == 0) {
+            f = f.map(e => e * (occ + 1));
+        } else {
+            for (let i = x; i <= r; i++) {
+                f[i] += f[i - x];
+                f[i] %= mod;
+            }
+            for (let i = r; i >= (occ + 1) * x; i--) {
+                f[i] -= f[i - (occ + 1) * x];
+                f[i] %= mod;
+            }
+        }
+    }
+    for (let i = l; i <= r; i++) {
+        res += f[i];
+        res %= mod;
+    }
+    return (res + mod) % mod;
+};
+///
 
 /* Breadth first search use Queue data structure. O(n)
 back ---queque---> front. Things get from the back and leave from front
