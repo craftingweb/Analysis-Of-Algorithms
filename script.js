82b198a8e23749bfe0bf710829889e7fde365f0f
From 0 to 1 <101225909+VitaliPri@users.noreply.github.com>
2023-11-26 19:08:18 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 ///
+var beautifulSubstrings = function(s, k) {
+    const n=s.length;
+    const vowels =new Set(['a','e','i','o','u']);
+    let i=1;
+    while (i*i%k!==0){
+        i++;
+    }
+    const q=i*2;
+    const p=Array.from({length:q},()=>({}));
+    p[q-1][0]=1;
+    let res=0;
+    let v=0;
+    for (let i=0;i<n;i++){
+        v+=vowels.has(s[i]) ?1:-1;
+        res += p[i%q][v]||0;
+        p[i%q][v]=(p[i%q][v]||0)+1;
+    }
+    return res;
+};
+
+///
 const counter = (a_or_s) => { let m = new Map(); for (const x of a_or_s) m.set(x, m.get(x) + 1 || 1); return m; };
 
 const mod = 1e9 + 7;
