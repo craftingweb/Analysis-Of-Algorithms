f9ea6869ad3c16ab5fbb26349e08ea3bcb2d50c9
From 0 to 1 <101225909+VitaliPri@users.noreply.github.com>
2023-12-07 23:37:44 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
+var invalidTransactions = function(transactions) {
+    const n = transactions.length;
+    const added = new Array(n).fill(false);
+    const res = [];
+
+    for (let i = 0; i < n; i++) {
+        const [name1, time1, amount1, city1] = transactions[i].split(",");
+        
+        if (amount1 > 1000 && !added[i]) {
+            res.push(transactions[i]);
+            added[i] = true;
+        }
+        
+        for (let j = i + 1; j < n; j++) {
+            const [name2, time2, amount2, city2] = transactions[j].split(",");
+            
+            if (name1 === name2 && Math.abs(time1 - time2) <= 60 && city1 != city2) {
+                if (!added[j]) {
+                   res.push(transactions[j]);
+                    added[j] = true;
+                }
+                
+                if (!added[i]) {
+                    res.push(transactions[i]);
+                    added[i] = true;
+                }
+            }
+        }
+        
+    }
+    
+    return res;
+};
 ///
 const ll = BigInt;
 
