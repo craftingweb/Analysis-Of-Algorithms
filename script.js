fb73f09b349c4d6e056017973380d94aea289b05
From 0 to 1 <101225909+VitaliPri@users.noreply.github.com>
2023-12-23 19:16:05 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+var maximumXorProduct = function(a, b, n) {
+  const mod = BigInt(1e9 + 7);
+  let ba = BigInt(a);
+  let bb = BigInt(b);
+  for (let i = BigInt(n - 1); i >= 0n; --i) {
+    const mask = 1n << i;
+    const am = ba & mask, bm = bb & mask;
+    if (!am && !bm) {
+      ba |= mask;
+      bb |= mask;
+    } else if ((!am && bm) || (am && !bm)) {
+      if (Math.abs(Number(ba - bb)) > Math.abs(Number((ba ^ mask) - (bb ^ mask)))) {
+        ba ^= mask;
+        bb ^= mask;
+      }
+    }
+  }
+  return Number((ba * bb) % mod);
+};
+//
 var kConcatenationMaxSum = function(arr, k) {
   var MOD = 1000000007;
   var totalSum = 0;
